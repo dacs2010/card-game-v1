a254f94b5008b86a95e19f4a834f7670f203080e
xXDuckXx <dacs2010@gmail.com>
2019-04-10 21:55:36 -0700

    selects individual cards and toggles their classes

--- app.js
+++ app.js
@@ -5,23 +5,14 @@ console.log("connected")
 // it is highlighted (click again to deselect)
 // click on a game board location to move the card to that spot
 
-
-
 function selectCard() {
-    document.addEventListener("click", function () {
-        console.log(event.srcElement, event.clientX,  event.clientY)
-
-    // highlight the target
-    target = document.querySelector(".target")
-    if (target.classList.contains("target")) {
-        target.classList.toggle("selected")
-    }
-    // move card to next mouse click location
-    if (target.classList.contains("selected")) {
-        target.style.top = `${event.clientY}px`
-        target.style.left = `${event.clientX}px`
-    }
-
+    targetCard = document.querySelectorAll('.target-card')
+    
+    targetCard.forEach(function(targetCard){
+        targetCard.addEventListener("click", function(){
+            console.log(targetCard)
+            targetCard.classList.toggle("selected")
+        })
     })
 }
 
